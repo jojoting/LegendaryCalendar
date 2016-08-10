286ad17550bccd919c9d524664c5287ab01264e8
tongzhou <tongzhou>
2016-08-10 22:27:31 +0800

    Initial Commit

---
+++ LegendaryCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LegendaryCalendar
+//
+//  Created by jojoting on 16/8/10.
+//  Copyright © 2016年 jojoting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LegendaryCalendar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LegendaryCalendar
+//
+//  Created by jojoting on 16/8/10.
+//  Copyright © 2016年 jojoting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
